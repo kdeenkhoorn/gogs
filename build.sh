196f3dec484a3f450892c451db22c02730e77d83
Klaas Eenkhoorn <klaas@odroid.int.kdedesign.nl>
2019-01-25 14:38:58 +0100

    Moved dockerfile to build-context directory.

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
 GOGSVER=0.11.79
 GOVER=1.11.4
 
+cd ./build-context
 docker build --network host --build-arg GOGSVER=${GOGSVER} --build-arg GOVER=${GOVER} -t kdedesign/gogs:${GOGSVER} .
 docker tag kdedesign/gogs:${GOGSVER} kdedesign/gogs:latest
