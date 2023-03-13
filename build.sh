734b185f6246f1a5c8d63c0557d1c04591e50ea5
Klaas Eenkhoorn <klaas@int.kdedesign.nl>
2023-03-13 13:14:40 +0100

    Updated to GOGS 0.13.0 and GO 1.20.2

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-GOGSVER=0.12.6
-GOVER=1.18
+GOGSVER=0.13.0
+GOVER=1.20.2
 
 cd ./build-context
 docker build --network host --build-arg GOGSVER=${GOGSVER} --build-arg GOVER=${GOVER} -t kdedesign/gogs:${GOGSVER} .
